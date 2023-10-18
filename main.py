ce293e8bc8438c3c0a6270669d379d5e2ec4dc18
AbahGabriel <gabeyiscool43@gmail.com>
2023-10-18 23:55:17 +0100

    Reduced use of multiple threads for each flair

--- main.py
+++ main.py
@@ -12,26 +12,10 @@ logging.basicConfig(
     filename="run.log"
 )
 
-config = config_util.load_config()
-reddit = reddit_util.initialize_reddit(config)
+CONFIG = config_util.load_config()
+REDDIT = reddit_util.initialize_reddit(CONFIG)
 
-def main():
-    subreddit_name = config['VARS']['SUBREDDIT']
-    subreddit = validate_subreddit(subreddit_name)
-    if not subreddit:
-        logging.error(f"Subreddit does not exist: r/{subreddit_name}.")
-        print(f"Subreddit does not exist: r/{subreddit_name}.")
-        exit()
-
-    if not subreddit.user_is_moderator:
-        logging.error(f"You must be a mod in this sub: r/{subreddit_name}.")
-        print(f"You must be a mod in this sub: r/{subreddit_name}.")
-        exit()
-
-    # Checks for and processes new posts.
-    print("Bot is now running...")
-    for post in subreddit.stream.submissions(skip_existing=True):
-        process_new_post(post)
+THREAD_LOCK = threading.Lock()
 
 # Checks if subreddit exists.
 def validate_subreddit(subreddit_name: str):
@@ -39,7 +23,7 @@ def validate_subreddit(subreddit_name: str):
         return None
     
     try:
-        return reddit.subreddits.search_by_name(subreddit_name, exact=True)[0]
+        return REDDIT.subreddits.search_by_name(subreddit_name, exact=True)[0]
     except prawcore.exceptions.NotFound:
         return None
     
@@ -51,50 +35,91 @@ def process_new_post(post: models.Submission):
     
     # Flair IDs to be applied based on how long the respective timers were
     # set to.
+    start_timer_flair_id = '07fdad48-5de4-11ee-9876-2e9c1667800a'
     short_timer_flair_id = 'd77946ce-566c-11ee-bea9-76fa97ad0426'
     medium_timer_flair_id = '4976c78e-566c-11ee-a694-ee0751b6e588'
     long_timer_flair_id = '1eb6c630-535a-11ee-92a7-4af16e60d54a'
 
-    threads = [] 
     try:
         if post.link_flair_template_id not in flair_ids_to_trigger_code_logic:
             return
 
-        # Creates multiple timers in separate threads to ensure that they all
-        # count down separately. I'll try to optimize this later, because the
-        # idea of multiple threads running like that kind of annoys me.
+        # Finally optimized the damn threads.
         print(f"New post to be processed: '{post.title}'")
         if post.link_flair_template_id == 'b730352e-5ace-11ee-bf0d-9e7c5190d2d0':
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(25, post, short_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(50, post, medium_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(75, post, long_timer_flair_id)))
+            time_to_flair_map = {
+                1: start_timer_flair_id,
+                25: short_timer_flair_id,
+                50: medium_timer_flair_id,
+                75: long_timer_flair_id
+            }
+
+            threading.Thread(target=update_flair_after_X_minutes, args=(post, time_to_flair_map)).start()
         elif post.link_flair_template_id == 'c1955a9e-5ace-11ee-acc1-ce042fb18b89':
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(50, post, short_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(75, post, medium_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(100, post, long_timer_flair_id)))
+            time_to_flair_map = {
+                1: start_timer_flair_id,
+                50: short_timer_flair_id,
+                75: medium_timer_flair_id
+            }
+
+            threading.Thread(target=update_flair_after_X_minutes, args=(post, time_to_flair_map)).start()
         elif post.link_flair_template_id == 'ca493cfa-5ace-11ee-bbe5-26d464526c47':
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(75, post, short_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(100, post, medium_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(125, post, long_timer_flair_id)))
+            time_to_flair_map = {
+                1: start_timer_flair_id,
+                75: short_timer_flair_id,
+                100: medium_timer_flair_id
+            }
+
+            threading.Thread(target=update_flair_after_X_minutes, args=(post, time_to_flair_map)).start()
         elif post.link_flair_template_id == 'dadc72c6-5ace-11ee-bb35-7e9b42ca2926':
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(100, post, short_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(125, post, medium_timer_flair_id)))
-            threads.append(threading.Thread(target=update_flair_after_X_minutes_pass, args=(150, post, long_timer_flair_id)))
+            time_to_flair_map = {
+                1: start_timer_flair_id,
+                100: short_timer_flair_id
+            }
+
+            threading.Thread(target=update_flair_after_X_minutes, args=(post, time_to_flair_map)).start()
     except AttributeError:
         return
 
-    # Starts all timers in their respective threads.
-    for thread in threads:
-        thread.start()
+def update_flair_after_X_minutes(post: models.Submission, time_to_flair_map: dict()):
+    # Gets the index of the current key-value pair in the dictionary.
+    for index, (duration, new_flair_id) in enumerate(time_to_flair_map.items()):
 
-def update_flair_after_X_minutes_pass(duration: int, post: models.Submission, new_flair_id: str):
-    logging.info(f"The flair of the post titled '{post.title}' will be updated after '{duration}' minute(s)")
-    time.sleep(duration * 60) # Multiply duration by 60 to wait in minutes.
-    
-    post.mod.flair(flair_template_id=new_flair_id) # Sets the new flair.
-    logging.info(f"Successfully updated the flair of the post titled '{post.title}' after '{duration}' minute(s)")
+        # Calculates how much longer the thread has to sleep to
+        # update the flairs accordingly.
+        current_duration = 0
+        if index == 0:
+            current_duration = duration
+        else:
+            current_duration = duration - list(time_to_flair_map.keys())[index - 1]
 
-    time.sleep(1) # Idk I just like how this looks.
+        time.sleep(current_duration * 60)
+        
+        try:
+            # Only one thread can update a post flair at any time.
+            with THREAD_LOCK:
+                post.mod.flair(flair_template_id=new_flair_id)
+
+            print(f"Post flair has been updated after '{duration}' minute(s) - '{post.title}' ")
+            logging.info(f"Successfully updated the flair of the post titled '{post.title}' after '{duration}' minute(s)")
+        except Exception as e:
+            logging.error(f"Failed to update the flair of the post titled '{post.title}' after '{duration}' minute(s). Error: {e}")
+            print(f"Error: {e}")
 
 if __name__ == '__main__':
-    main()
+    subreddit_name = CONFIG['VARS']['SUBREDDIT']
+    subreddit = validate_subreddit(subreddit_name)
+    if not subreddit:
+        logging.error(f"Subreddit does not exist: r/{subreddit_name}.")
+        print(f"Subreddit does not exist: r/{subreddit_name}.")
+        exit()
+
+    if not subreddit.user_is_moderator:
+        logging.error(f"You must be a mod in this sub: r/{subreddit_name}.")
+        print(f"You must be a mod in this sub: r/{subreddit_name}.")
+        exit()
+
+    print("Bot is now running...")
+    logging.info("Bot has started running.")
+    for post in subreddit.stream.submissions(skip_existing=True):
+        process_new_post(post)
